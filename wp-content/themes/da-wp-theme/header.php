7796d8e55639d90905a28aa54ae76ae0d0b2e1dc
pacster <martinez@raidho.mx>
2015-11-16 23:07:19 -0600

    Se añaden hojas de estilos complementarias (existentes)

--- wp-content/themes/da-wp-theme/header.php
+++ wp-content/themes/da-wp-theme/header.php
@@ -4,6 +4,14 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
+		<link rel="stylesheet" type="text/css" href="<?php echo "$main_url"; ?>/css/templates.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo "$main_url"; ?>/css/type.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo "$main_url"; ?>/css/sidebar.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo "$main_url"; ?>/css/queries.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo "$main_url"; ?>/css/responsive-nav.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo "$main_url"; ?>/css/jquery.desoslide.min.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo "$main_url"; ?>/css/animate.min.css" />
+
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
