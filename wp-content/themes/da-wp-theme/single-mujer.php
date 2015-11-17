862e42b5839bae6c084d3ee36401be3671d23e84
pacster <martinez@raidho.mx>
2015-11-17 00:27:26 -0600

    Se genera single-mujer con llamada a campos ACF exitosa

--- wp-content/themes/da-wp-theme/single-mujer.php
+++ wp-content/themes/da-wp-theme/single-mujer.php
@@ -4,43 +4,71 @@
 	<!-- section -->
 	<section>
 
+	<div id="content" class="single_product">
+
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<div class="product_info">
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+				<div class="product_image">
+					<ul class="thumbs">
+						<li></li>
+					</ul>
+					<div id="slideshow">
+						<!-- post thumbnail -->
+						<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+								<?php the_post_thumbnail(); // Fullsize image for the single post ?>
+						<?php endif; ?>
+						<!-- /post thumbnail -->
+					</div>
+				</div>
 
-			<?php the_content(); // Dynamic Content ?>
+				<div class="product_description">
 
-		</article>
-		<!-- /article -->
+					<h2><?php the_title(); ?></h2>
+					<?php
+					// check if the repeater field has rows of data
+					if( have_rows('detalles_prenda_individual') ):
+						// loop through the rows of data
+						while ( have_rows('detalles_prenda_individual') ) : the_row();
+					?>
+					<?php the_sub_field('descripcion_prenda'); ?>
+					<p><strong>$<?php the_sub_field('precio'); ?>.00 mxn</strong></p>
+					<p><?php the_sub_field('tallas'); ?></p>
+
+					<?php if( get_sub_field('liga_kichink') ): ?>
+						<p><a class="buy_button" href="<?php the_sub_field('liga_kichink'); ?>">Comprar en Kichink</a></p>
+					<?php endif; ?>
+
+					<?php endwhile;
+
+					else :
+						echo 'No rows found';
+					endif;
+					?>
+
+				</div>
+
+			</article>
+			<!-- /article -->
+
+		</div>
+		<!-- /product info-->
 
 	<?php endwhile; ?>
-
 	<?php else: ?>
-
 		<!-- article -->
 		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
+			<h1><?php _e( 'No se encontró contenido.', 'html5blank' ); ?></h1>
 		</article>
 		<!-- /article -->
-
 	<?php endif; ?>
+
+	</div>
+	<!-- /#content -->
 
 	</section>
 	<!-- /section -->
