a48b2678e19108fc653a817e7f9e8dd33ff1e8ec
pacster <martinez@raidho.mx>
2015-11-18 23:25:57 -0600

    Se agregan todos los campos de configuración al home

--- wp-content/themes/da-wp-theme/index.php
+++ wp-content/themes/da-wp-theme/index.php
@@ -8,31 +8,87 @@
 
 				<div id="featured_slider" class="col3">
 					<ul>
-						<li style="background-image:url(img/feat-banner7.jpg);">
-							<h2><a href="<?php echo home_url(); ?>/colecciones/daisy/camp">Dâisy: Spring Collection<br> <span>Ver Campaña.</span></a></h2>
+						<li style="background-image:url(<?php the_field('home-main-featured', 'option'); ?>);">
+							<h2><a href="<?php the_field('home-main-featured-link-url', 'option'); ?>"><?php the_field('home-main-featured-caption', 'option'); ?><br> <span><?php the_field('home-main-featured-link', 'option'); ?></span></a></h2>
 						</li>
 					</ul>
 				</div>
-				<div class="col1" style="background-image:url(img/home/home1a.gif);">
-					<p><a href="<?php echo home_url(); ?>/mujer/02-crochet-lace-v-playsuit.php"><span>Crochet Lace 'V' Playsuit</span><br>$850</a></p>
+				<div
+				class="col1"
+				style="background-image:url(
+					<?php the_field('home-grid-a', 'option'); ?>
+					);">
+					<p>
+						<a href="<?php the_field('home-grid-a-link', 'option'); ?>">
+						<?php the_field('home-grid-a-txt', 'option'); ?></a>
+					</p>
 				</div>
-				<div class="col2" style="background-image:url(img/home/home1b.jpg);">
-					<p><a href="<?php echo home_url(); ?>/mujer/06-cut-out-mixed-dress.php"><span>Cut Out Mixed Dress</span><br>$999</a></p>
+
+				<div
+				class="col2"
+				style="background-image:url(
+					<?php the_field('home-grid-b', 'option'); ?>
+					);">
+					<p>
+						<a href="<?php the_field('home-grid-b-link', 'option'); ?>">
+						<?php the_field('home-grid-b-txt', 'option'); ?></a>
+					</p>
 				</div>
-				<div class="col1" style="background-image:url(img/home/home2a.jpg);">
-					<p><a href="<?php echo home_url(); ?>/mujer/04-light-floral-crochet-dress.php"><span>Light Floral Crochet Dress</span><br>$980</a></p>
+
+				<div
+				class="col1"
+				style="background-image:url(
+					<?php the_field('home-grid-c', 'option'); ?>
+					);">
+					<p>
+						<a href="<?php the_field('home-grid-c-link', 'option'); ?>">
+						<?php the_field('home-grid-c-txt', 'option'); ?></a>
+					</p>
 				</div>
-				<div class="col1" style="background-image:url(img/home/home2b.jpg);">
-					<h2><a href="http://dawearhouse.us3.list-manage2.com/subscribe?u=1984dddcac388f6ae9e237b73&id=8c765529fe" target="_blank"><span>Dâ Wearhouse Weekly:</span> Suscríbete al Newsletter.</a></h2>
+
+				<!-- Newsletter -->
+				<div
+				class="col1"
+				style="background-image:url(
+					<?php the_field('home-newsletter-image', 'option'); ?>
+					);">
+					<p>
+						<h2><a href="<?php the_field('home-newsletter-link-url', 'option'); ?>">
+						<?php the_field('home-newsletter-link', 'option'); ?></a></h2>
+					</p>
 				</div>
-				<div class="col1" style="background-image:url(img/home/home2c.jpg);">
-					<p><a href="<?php echo home_url(); ?>/mujer/07-frill-bardot-shoulder-dress.php"><span>Frill bardot Shoulder Dress</span><br>$1,499</a></p>
+
+				<div
+				class="col1"
+				style="background-image:url(
+					<?php the_field('home-grid-d', 'option'); ?>
+					);">
+					<p>
+						<a href="<?php the_field('home-grid-d-link', 'option'); ?>">
+						<?php the_field('home-grid-d-txt', 'option'); ?></a>
+					</p>
 				</div>
-				<div class="col2" style="background-image:url(img/home/home3a.jpg);">
-					<p><a href="<?php echo home_url(); ?>/mujer/01-two-lace-bandeau-dress.php"><span>Two Lace Bandeau Dress</span><br>$899</a></p>
+
+				<div
+				class="col2"
+				style="background-image:url(
+					<?php the_field('home-grid-e', 'option'); ?>
+					);">
+					<p>
+						<a href="<?php the_field('home-grid-e-link', 'option'); ?>">
+						<?php the_field('home-grid-e-txt', 'option'); ?></a>
+					</p>
 				</div>
-				<div class="col1" style="background-image:url(img/home/home3b.gif);">
-					<p><a href="<?php echo home_url(); ?>/mujer/03-metallic-white-tulle-dress.php"><span>Metallic white tulle Dress</span><br>$950</a></p>
+
+				<div
+				class="col1"
+				style="background-image:url(
+					<?php the_field('home-grid-f', 'option'); ?>
+					);">
+					<p>
+						<a href="<?php the_field('home-grid-f-link', 'option'); ?>">
+						<?php the_field('home-grid-f-txt', 'option'); ?></a>
+					</p>
 				</div>
 
 			</div>
